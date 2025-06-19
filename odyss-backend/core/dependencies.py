888c2ecd0515868af9fee683f0a7db36a718f94a
Izumitempest <lilice308@gmail.com>
2025-06-19 13:26:41 +0100

    fix: store the current user in the global context for easier access

--- odyss-backend/core/dependencies.py
+++ odyss-backend/core/dependencies.py
@@ -1,3 +1,4 @@
+from flask import g
 from flask_jwt_extended import get_jwt_identity
 from app.extensions import db
 from models.user import User
@@ -6,8 +7,8 @@ def get_current_user():
     user_id = get_jwt_identity()
     if not user_id:
         raise Exception("No user identity found")
-    # Use db.session to keep User in session
     user = db.session.query(User).get(user_id)
     if not user:
         raise Exception("User not found")
+    g.user = user
     return user
